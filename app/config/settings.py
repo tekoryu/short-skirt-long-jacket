2b7afe55e99ae4f598bf948799f8ae19b387315e
Your Name <you@example.com>
2025-11-25 22:12:27 -0300

    Add Whitenoise for static file management in Django
    - Added `whitenoise` to `requirements.txt` for improved static file handling.
    - Updated `settings.py` to include `WhiteNoiseMiddleware` in the middleware stack and configured `CompressedManifestStaticFilesStorage` for static files.
    
    This commit enhances static file serving in production, improving performance and reliability.

--- app/config/settings.py
+++ app/config/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -153,6 +154,12 @@ STATIC_ROOT = os.environ.get('STATIC_ROOT', '/vol/web/static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/vol/web/media')
